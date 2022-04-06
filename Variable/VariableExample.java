effd4766ee3f149cbc3680e94cb97e97a987272b
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-04-06 22:36:41 +0530

    Variable code added

---
+++ Variable/VariableExample.java
@@ -0,0 +1,15 @@
+public class VariableExample {
+	public static void main(String[] args) {
+    String name = "James Gosling";  //string
+    int yearCreated = 1995;         //int
+    double androidShare = 81.7;     //double
+    boolean intsCanHoldDecimals = false;    //boolean
+    char expectedGrade = 'A';   //char
+    System.out.println(androidShare);
+    System.out.println(name);
+    System.out.println(yearCreated);
+    System.out.println(intsCanHoldDecimals);
+    System.out.println(expectedGrade);
+
+	}
+}
