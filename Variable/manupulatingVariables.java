eda7b4031f051e6f82e50f7e637e428b9f749dbe
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-04-10 23:45:40 +0530

    manupulating variables

---
+++ Variable/manupulatingVariables.java
@@ -0,0 +1,11 @@
+ublic class manupulatingVariable {
+	public static void main(String[] args) {   
+// 		We can use compound assignment operators for all of the arithmetic operators we’ve covered:
+
+// Addition (+=)
+// Subtraction (-=)
+// Multiplication (*=)
+// Division (/=)
+// Modulo (%=)
+	}
+}
