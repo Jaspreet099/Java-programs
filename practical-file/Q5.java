97acee2656cec21a589f56cef008612ff05d1c59
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-04-24 01:00:50 +0530

    Practical Question

---
+++ practical-file/Q5.java
@@ -0,0 +1,22 @@
+class Q5{
+    public static void main(String[]args){
+        int num1 = 36, num2= 80, num3=69;
+        if(num1>num2){
+            if(num1>= num3){
+                System.out.println(num1 + "is largest number");
+            }
+            else{
+                System.out.println(num3 + "is largest number");
+            }
+        }
+
+        else{
+            if(num2>= num3){
+                System.out.println(num2 + "is largest number");
+            }
+             else{
+                System.out.println(num3 + "is largest number");
+            }
+        }
+    }
+}
