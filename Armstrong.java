7ce5b1369caeb4d3d0f64af5765ad743ce28e253
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-05-22 20:51:13 +0530

    Armstrong number code added

---
+++ Armstrong.java
@@ -0,0 +1,19 @@
+class Armstrong{
+        public static void main(String[] args){
+            int num= 153, number, temp, total = 0;
+
+            number = num;
+
+            while(number != 0){
+                temp = number % 10;
+                total = total + (temp*temp*temp);
+                number /= 10;
+            }
+            if(total == num){
+                System.out.println(num + "is an armstrong number");
+            }
+            else{
+                System.out.println(num + "is not an armstrong number");
+            }
+        }
+}
---
+++ practical-file/Q10.java
@@ -0,0 +1,23 @@
+public class Q10  
+{              
+public static void main(String[] args)   
+{  
+for (int i = 1; i <= 4; i++)  
+{  
+int n = 8;  
+for (int j = 1; j<= n - i; j++)   
+{   
+System.out.print(" ");   
+}   
+for (int k = i; k >= 1; k--)  
+{  
+System.out.print(k);  
+}  
+for (int l = 2; l <= i; l++)   
+{  
+System.out.print(l);   
+}   
+System.out.println();   
+}   
+}
+}
